64d1a1dca71ec96130b1b0764b2fde48d9bb84aa
Lin Bing Lin <bing40081@gmai.com>
2022-11-01 15:34:43 +0800

    test

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def user_set():
+    return render_template('about.html')
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=True)
